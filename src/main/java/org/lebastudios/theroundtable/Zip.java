cf51644448e63a415f6c6c75d2352d9acf3145f4
Borja Castellano <borjacastellano1@gmail.com>
2024-10-14 22:24:06 +0200

    (Backup DB) Backup system for the databases implemented

--- src/main/java/org/lebastudios/theroundtable/Zip.java
+++ src/main/java/org/lebastudios/theroundtable/Zip.java
@@ -11,27 +11,26 @@ public class Zip
 {
     public static void createZip(File source, File destination)
     {
-        try (var fos = new FileOutputStream(destination);
-             var zos = new ZipOutputStream(fos))
+        try (var zos = new ZipOutputStream(new FileOutputStream(destination)))
         {
             var buffer = new byte[1024];
             var files = source.listFiles();
+            assert files != null;
             for (var file : files)
             {
-                var fis = new FileInputStream(file);
-                zos.putNextEntry(new ZipEntry(file.getName()));
-                int length;
-                while ((length = fis.read(buffer)) > 0)
+                try (var fis = new FileInputStream(file))
                 {
-                    zos.write(buffer, 0, length);
+                    zos.putNextEntry(new ZipEntry(file.getName()));
+                    int length;
+                    while ((length = fis.read(buffer)) > 0)
+                    {
+                        zos.write(buffer, 0, length);
+                    }
+                    zos.closeEntry();
                 }
-                zos.closeEntry();
-                fis.close();
+                catch (Exception _) {}
             }
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        catch (IOException _) {throw new RuntimeException("Failed to create zip file.");}
     }
 }
