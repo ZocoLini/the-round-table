200f6929de4d4668db5f95749b3e3e45fdbd47f2
Borja Castellano <borjacastellano1@gmail.com>
2024-10-20 16:18:11 +0200

    (UI) First step to update the UI and make it cooler

---
+++ src/main/java/org/lebastudios/theroundtable/ui/IconTextButton.java
@@ -0,0 +1,59 @@
+package org.lebastudios.theroundtable.ui;
+
+import javafx.scene.control.Button;
+import lombok.Getter;
+
+@Getter
+public class IconTextButton extends Button
+{
+    private int iconSize = 20;
+
+    private String iconName;
+
+    public IconTextButton()
+    {
+        this("");
+    }
+
+    public IconTextButton(String iconName)
+    {
+        super();
+
+        this.iconName = iconName;
+
+        updateIcon();
+    }
+
+    private void updateIcon()
+    {
+        if (iconName == null || iconName.isEmpty()) 
+        {
+            this.setGraphic(null);
+            return;
+        }
+        
+        IconView iconView = new IconView(iconName);
+
+        iconView.setFitWidth(iconSize);
+        iconView.setFitHeight(iconSize);
+
+        this.setGraphic(iconView);
+
+        this.setPrefSize(iconSize, iconSize);
+        this.setMinSize(iconSize, iconSize);
+        this.setMaxSize(iconSize, iconSize);
+    }
+
+    public void setIconSize(int iconSize)
+    {
+        this.iconSize = iconSize;
+        updateIcon();
+    }
+
+    public void setIconName(String iconName)
+    {
+        this.iconName = iconName;
+        updateIcon();
+    }
+}
+
