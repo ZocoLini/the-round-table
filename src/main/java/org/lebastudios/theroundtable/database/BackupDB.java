a345a3d5400b4907db5107b2cabeeff079d6d7be
Borja Castellano <borjacastellano1@gmail.com>
2024-10-30 20:32:40 +0100

    (Database) Debugged the backup number limitation

--- src/main/java/org/lebastudios/theroundtable/database/BackupDB.java
+++ src/main/java/org/lebastudios/theroundtable/database/BackupDB.java
@@ -5,11 +5,13 @@ import org.lebastudios.theroundtable.AppTask;
 import org.lebastudios.theroundtable.Zip;
 import org.lebastudios.theroundtable.config.data.DatabaseConfigData;
 import org.lebastudios.theroundtable.config.data.JSONFile;
+import org.lebastudios.theroundtable.dialogs.InformationTextDialogController;
 import org.lebastudios.theroundtable.events.AppLifeCicleEvents;
 import org.lebastudios.theroundtable.ui.TaskManager;
 
 import java.io.File;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
@@ -48,7 +50,7 @@ public class BackupDB
         try
         {
             executor = Executors.newSingleThreadScheduledExecutor();
-            executor.scheduleAtFixedRate(this::realizeBackup, 1, 1, TimeUnit.HOURS);
+            executor.scheduleAtFixedRate(this::realizeBackup, 0, 1, TimeUnit.HOURS);
         }
         catch (Exception exception) {}
     }
@@ -85,34 +87,37 @@ public class BackupDB
 
                 if (!backupFolder.exists() && !backupFolder.mkdirs())
                 {
-                    updateMessage("Failed to create backup directory.");
+                    InformationTextDialogController.loadAttachedNode("DATABASE ERROR: Failed to create backup directory.");
                     updateProgress(100, 100);
                     return null;
                 }
 
                 if (!databaseFolder.exists() && !databaseFolder.mkdirs())
                 {
-                    updateMessage("Failed to create database directory.");
+                    InformationTextDialogController.loadAttachedNode("DATABASE ERROR: Failed to create database directory.");
                     updateProgress(100, 100);
                     return null;
                 }
-
-                // TODO: Validate that this works as expected
                 
                 final var backupsCreated = Objects.requireNonNull(backupFolder.listFiles());
                 
                 int numMaxBackups = new JSONFile<>(DatabaseConfigData.class).get().numMaxBackups;
                 
-                if (backupsCreated.length > numMaxBackups) 
+                if (backupsCreated.length >= numMaxBackups) 
                 {
                     Arrays.stream(backupsCreated)
                             .filter(file -> file.isFile())
-                            .sorted(Comparator.comparing(a -> LocalDateTime.parse(a.getName())))
-                            .skip(numMaxBackups)
+                            .sorted(Comparator.reverseOrder())
+                            .skip(numMaxBackups - 1)
                             .forEach(file -> file.delete());
                 }
+
+                updateProgress(50, 100);
                 
-                File backupFile = new File(backupFolder.getAbsolutePath(), LocalDateTime.now() + ".zip");
+                File backupFile = new File(
+                        backupFolder.getAbsolutePath(), 
+                        LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS) + ".zip"
+                );
 
                 try
                 {
