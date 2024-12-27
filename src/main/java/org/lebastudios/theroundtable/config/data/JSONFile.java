661a7ab06e2c5baff38162a33eb342b99df40aa6
Borja Castellano <borjacastellano1@gmail.com>
2024-12-27 19:20:41 +0100

    (2.3.8) The JSONFile no longer has a cache

--- src/main/java/org/lebastudios/theroundtable/config/data/JSONFile.java
+++ src/main/java/org/lebastudios/theroundtable/config/data/JSONFile.java
@@ -2,22 +2,12 @@ package org.lebastudios.theroundtable.config.data;
 
 import com.google.gson.GsonBuilder;
 import lombok.SneakyThrows;
-import org.hibernate.cache.CacheException;
-import org.lebastudios.theroundtable.events.UserEvents;
 
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JSONFile<T extends FileRepresentator>
 {
-    private static final Map<Class<?>, Object> cache = new HashMap<>();
-
-    static {
-        UserEvents.OnAccountLogOutBefore.addListener(_ -> cache.clear());
-    }
-    
     private final T data;
 
     public JSONFile(Class<T> clazz)
@@ -33,24 +23,11 @@ public class JSONFile<T extends FileRepresentator>
     @SneakyThrows
     private <T extends FileRepresentator> T load(Class<T> clazz)
     {
-        if (cache.containsKey(clazz))
-        {
-            var cached = cache.get(clazz);
-            
-            if (cached.getClass().equals(clazz)) return (T) cache.get(clazz);
-
-            throw new CacheException("The cached object is not of the same class as the requested one.");
-        }
-
         var file = clazz.getConstructor().newInstance().getFile();
 
         if (!file.exists()) return clazz.getConstructor().newInstance();
 
-        var fileContent = new GsonBuilder().create().fromJson(new FileReader(file), clazz);
-
-        cache.put(clazz, fileContent);
-
-        return fileContent;
+        return new GsonBuilder().create().fromJson(new FileReader(file), clazz);
     }
 
     public T get()
