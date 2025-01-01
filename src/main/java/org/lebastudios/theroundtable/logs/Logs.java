830331d970b975f10849b400765d14469942046c
Borja Castellano <borjacastellano1@gmail.com>
2025-01-01 13:04:33 +0100

    (Dev. 3.0.0-SNAPSHOT -> Dev. 2.5.0) Canceling port to Android update

--- src/main/java/org/lebastudios/theroundtable/logs/Logs.java
+++ src/main/java/org/lebastudios/theroundtable/logs/Logs.java
@@ -1,5 +1,7 @@
 package org.lebastudios.theroundtable.logs;
 
+import java.time.format.DateTimeFormatter;
+
 public class Logs
 {
     private static Logs instance;
@@ -13,4 +15,16 @@ public class Logs
     }
     
     private Logs() {}
+    
+    public void log(LogType type, String message)
+    {
+        String date = java.time.LocalDateTime.now().format(DateTimeFormatter.ofPattern("O dd/MM/yyyy HH:mm:ss"));
+        
+        switch (type)
+        {
+            case INFO -> System.out.println(date + " [INFO] " + message);
+            case WARNING -> System.out.println(date + " [WARNING] " + message);
+            case ERROR -> System.err.println(date + " [ERROR] " + message);
+        }
+    }
 }
