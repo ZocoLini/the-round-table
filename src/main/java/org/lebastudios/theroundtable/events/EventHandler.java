973cfeb9ca1bb2b611394de9d2fb40cecadfd117
Borja Castellano <borjacastellano1@gmail.com>
2025-01-09 22:23:40 +0100

    (Dev. 2.5.0) Added some debugging info about events in the EventHandler.java

--- src/main/java/org/lebastudios/theroundtable/events/EventHandler.java
+++ src/main/java/org/lebastudios/theroundtable/events/EventHandler.java
@@ -1,5 +1,7 @@
 package org.lebastudios.theroundtable.events;
 
+import org.lebastudios.theroundtable.env.Variables;
+
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,53 +18,74 @@ public abstract class EventHandler<T>
 {
     private final List<T> listeners = new CopyOnWriteArrayList<>();
     private final List<WeakReference<T>> weakListeners = new CopyOnWriteArrayList<>();
-    
-    public synchronized void addListener(T listener) {
-        if (listener == null) {
+
+    public synchronized void addListener(T listener)
+    {
+        if (listener == null)
+        {
             throw new IllegalArgumentException("Listener cannot be null");
         }
 
-        if (!hasListener(listener)) {
+        if (!hasListener(listener))
+        {
             listeners.add(listener);
         }
     }
 
-    public synchronized void addWeakListener(T listener) {
-        if (listener == null) {
+    public synchronized void addWeakListener(T listener)
+    {
+        if (listener == null)
+        {
             throw new IllegalArgumentException("Listener cannot be null");
         }
 
-        if (!hasListener(listener)) {
+        if (!hasListener(listener))
+        {
             weakListeners.add(new WeakReference<>(listener));
         }
     }
-    
-    public synchronized void removeListener(T listener) {
+
+    public synchronized void removeListener(T listener)
+    {
         if (listener == null) return;
 
-        if (!listeners.remove(listener)) 
+        if (!listeners.remove(listener))
         {
             weakListeners.removeIf(w -> w.get() == listener);
-        };
+        }
+        ;
     }
 
-    public synchronized void clearListeners() {
+    public synchronized void clearListeners()
+    {
         listeners.clear();
     }
 
-    public boolean hasListener(T listener) {
+    public boolean hasListener(T listener)
+    {
         if (listener == null) return false;
 
         return listeners.contains(listener) || weakListeners.stream().anyMatch(w -> w.get() == listener);
     }
 
-    protected List<T> getActiveListeners() {
+    public void removeNullListeners()
+    {
         listeners.removeIf(Objects::isNull);
         weakListeners.removeIf(w -> w.get() == null);
-        
+    }
+
+    protected List<T> getActiveListeners()
+    {
+        removeNullListeners();
+
         var list = new ArrayList<>(listeners);
         list.addAll(weakListeners.stream().map(WeakReference::get).toList());
-        
+
+        if (Variables.isDev())
+        {
+            System.out.println("Active Listeners: " + list.size());
+        }
+
         return list;
     }
 }
