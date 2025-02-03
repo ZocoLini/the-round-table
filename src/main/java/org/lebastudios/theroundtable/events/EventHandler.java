225a199c5da10d74944dc9d31f62e2f1d8affd71
Borja Castellano <borjacastellano1@gmail.com>
2025-02-03 22:46:03 +0100

    (plugin-receipt-manager) (Dev. 1.4.0) Removed unnecessary listeners copy in the event system

--- src/main/java/org/lebastudios/theroundtable/events/EventHandler.java
+++ src/main/java/org/lebastudios/theroundtable/events/EventHandler.java
@@ -1,12 +1,11 @@
 package org.lebastudios.theroundtable.events;
 
+import lombok.NonNull;
 import org.lebastudios.theroundtable.env.Variables;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * This class is used for the event system. Defines the methods that all AppEvents must have.
@@ -16,67 +15,52 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public abstract class EventHandler<T>
 {
-    private final List<T> listeners = new CopyOnWriteArrayList<>();
-    private final List<WeakReference<T>> weakListeners = new CopyOnWriteArrayList<>();
-
-    public synchronized void addListener(T listener)
+    private final List<T> listeners = new ArrayList<>();
+    private final List<WeakReference<T>> weakListeners = new ArrayList<>();
+    
+    public synchronized void addListener(@NonNull T listener)
     {
-        if (listener == null)
-        {
-            throw new IllegalArgumentException("Listener cannot be null");
-        }
-
         if (!hasListener(listener))
         {
             listeners.add(listener);
         }
     }
 
-    public synchronized void addWeakListener(T listener)
+    public synchronized void addWeakListener(@NonNull T listener)
     {
-        if (listener == null)
-        {
-            throw new IllegalArgumentException("Listener cannot be null");
-        }
-
         if (!hasListener(listener))
         {
             weakListeners.add(new WeakReference<>(listener));
         }
     }
 
-    public synchronized void removeListener(T listener)
+    public synchronized void removeListener(@NonNull T listener)
     {
-        if (listener == null) return;
-
         if (!listeners.remove(listener))
         {
             weakListeners.removeIf(w -> w.get() == listener);
         }
-        ;
     }
 
+    public synchronized void removeWeakListener(@NonNull T listener)
+    {
+        weakListeners.removeIf(w -> w.get() == listener);
+    }
+    
     public synchronized void clearListeners()
     {
         listeners.clear();
+        weakListeners.clear();
     }
 
-    public boolean hasListener(T listener)
+    public boolean hasListener(@NonNull T listener)
     {
-        if (listener == null) return false;
-
-        return listeners.contains(listener) || weakListeners.stream().anyMatch(w -> w.get() == listener);
+        return listeners.contains(listener) 
+                || weakListeners.stream().anyMatch(w -> w.get() == listener);
     }
-
-    public void removeNullListeners()
-    {
-        listeners.removeIf(Objects::isNull);
-        weakListeners.removeIf(w -> w.get() == null);
-    }
-
     protected List<T> getActiveListeners()
     {
-        removeNullListeners();
+        weakListeners.removeIf(w -> w.get() == null);
 
         var list = new ArrayList<>(listeners);
         list.addAll(weakListeners.stream().map(WeakReference::get).toList());
